f7240f33fbf130844b4a6f4054eb2ff5dda6b0b7
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-09-27 15:41:06 -0300

    fazendo algumas alterações e tornando o código genérico

--- receita7_funcaoFetch/script.js
+++ receita7_funcaoFetch/script.js
@@ -1,28 +1,30 @@
-const carregarDiv = cs =>{
-    const div = document.getElementById("cervejasDiv")
-    const itensHtml = cs.map(({id,bank_name,account_number}) =>`
-        <tr>
-            <td>
-                ${id}
-            </td>
-            <td>
-                ${bank_name}  
-            </td>
-            <td>
-                ${account_number}  
-            </td>
-        </tr>`
-    )
-    div.innerHTML = `
-
-    <table border="1" class = "tabela1">
-        <tr>                
-                <th>Id</th>                    
-                <th>Nome do Banco</th>
-                <th>Número da Conta</th>    
-            ${itensHtml.join("\n")}
-        </tr>
-    </table>`  
-    
+async function carregarTabela(link) {
+    try {
+        const response = await fetch(link);
+        const data = await response.json();
+        if (data && data.length > 0) {
+            const tableHtml = criarTabela(data, ["id", "bank_name","account_number"],["Id","Nome do banco","Número da conta"]); // Especifique as colunas que você deseja exibir
+            tabelaDiv.innerHTML = tableHtml;
+        } else {
+            tabelaDiv.innerHTML = "Nenhum dado encontrado.";
+        }
+    } catch (err) {
+        tabelaDiv.innerHTML = "Erro ao carregar os dados.";
+        console.error(err);
+    }
 }
 
+function criarTabela(data, columns,titulos) {
+    const titulosHtml = titulos.map(titulos => `<th>${titulos}</th>`).join("");
+    const rowsHtml = data.map(item => {
+        const rowHtml = columns.map(column => `<td>${item[column]}</td>`).join("");
+        return `<tr>${rowHtml}</tr>`;
+    }).join("");
+
+    return `
+        <table border="1" class="tabela1">
+            <tr>${titulosHtml}</tr>
+            ${rowsHtml}
+        </table>
+    `;
+}
