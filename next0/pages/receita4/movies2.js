0e444f570ab59a444f736fbdf461768e01bd9f0e
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-19 21:53:18 -0300

    finalizando teoria da receita 4 e utilizando hooks para printar 'carregando' enquanto ocorre o await do fetch

---
+++ next0/pages/receita4/movies2.js
@@ -0,0 +1,24 @@
+import useSWR from "swr"
+
+async function fetcher (url){
+    const res = await fetch (url)
+    const json = await res.json()
+    return json
+}
+
+export default function Movies2(){
+    const {data,error} = useSWR("http://www.omdbapi.com/?apikey=4e779fd4&s=bagdad",fetcher)
+    if (error) return <div>Falha na reprodução...</div>
+    if(!data) return <div>Carregando...</div>
+
+    return (
+
+        <div>
+
+                { data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  ) }         
+
+        </div>
+
+    )
+
+}
