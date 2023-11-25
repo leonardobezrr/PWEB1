ddc74ec2f398950e9ef2ad4135e918bb00237b56
Leonardo Bezerra <leonardobezerra05@gmail.com>
2023-11-24 23:52:35 -0300

    interligando com site externo e pegando informações dos filmes a partir do código do imdbID

--- next0/pages/receita4/movies2.js
+++ next0/pages/receita4/movies2.js
@@ -1,24 +1,29 @@
-import useSWR from "swr"
+import useSWR from "swr";
 
-async function fetcher (url){
-    const res = await fetch (url)
-    const json = await res.json()
-    return json
+async function fetcher(url) {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
 }
 
-export default function Movies2(){
-    const {data,error} = useSWR("http://www.omdbapi.com/?apikey=4e779fd4&s=bagdad",fetcher)
-    if (error) return <div>Falha na reprodução...</div>
-    if(!data) return <div>Carregando...</div>
+export default function Movies2() {
+  const { data, error } = useSWR(
+    "http://www.omdbapi.com/?apikey=4e779fd4&s=bagdad",
+    fetcher
+  );
 
-    return (
+  if (error) return <div>Falha na reprodução...</div>;
+  if (!data) return <div>Carregando...</div>;
 
-        <div>
-
-                { data.Search.map( (m) => <div><a href="">{m.Title} --- {m.Year}</a></div> ) }         
-
+  return (
+    <div>
+      {data.Search.map((m) => (
+        <div key={m.imdbID}>
+          <a href={`http://www.imdb.com/title/${m.imdbID}`} target="_blank" rel="noopener noreferrer">
+            {m.Title} --- {m.Year}
+          </a>
         </div>
-
-    )
-
+      ))}
+    </div>
+  );
 }
